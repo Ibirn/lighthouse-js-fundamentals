abca943c47611378edd6f4f72707bcfa50270d65
Ibirn <kmcricciuti@gmail.com>
2020-07-22 20:31:07 +0000

    just keeping it uptodate, even though it's not important.

--- tinkering.js
+++ tinkering.js
@@ -1,1 +1,11 @@
-!true
+const whichSchool  = function (age) {
+  if (age < 13){
+    console.log("Go to Elementary School");
+  }
+  else if (age >= 13 && age <= 18){
+    console.log("Go to Secondary School");
+  }
+  else {
+    console.log("Go to LHL.")
+  }
+}
