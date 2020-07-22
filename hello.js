78021bdba873d61aa8c8a6c8a5549532f74ac299
Ibirn <kmcricciuti@gmail.com>
2020-07-22 23:28:28 +0000

    A brief hello from the void

---
+++ hello.js
@@ -0,0 +1,13 @@
+/*const sayhello = function(){
+  console.log("Hello, world!");
+}
+
+sayhello();
+*/
+const sayHello = function(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Xanthadar");
+sayHello("Bill");
+sayHello("Cromulent");
