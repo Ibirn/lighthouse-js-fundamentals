ff5ccf433c75a88cfb6cf186a02a46064d97f3aa
Ibirn <kmcricciuti@gmail.com>
2020-07-23 18:51:21 +0000

    remembering concatenation

---
+++ concatArrays.js
@@ -0,0 +1,6 @@
+let concat = (arrayOne, arrayTwo) => {
+  let newArr = arrayOne.concat(arrayTwo)
+  return newArr;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]));
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]));
